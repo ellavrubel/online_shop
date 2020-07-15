9503592ad4793a82ee0f0b241f4d6462ee9d3c5d
ellavrubel <webella@protonmail.com>
2020-07-15 15:40:21 +0300

    header finished

--- gulpfile.js
+++ gulpfile.js
@@ -39,9 +39,10 @@
         });
 
         gulp.task('js', function () {
-            return gulp.src(
-                'node_modules/slick-carousel/slick/slick.js'
-            )
+            return gulp.src([
+                'node_modules/slick-carousel/slick/slick.js',
+                'node_modules/jquery/dist/jquery.min.js'
+            ])
                 .pipe(concat('libs.min.js'))
                 .pipe(uglify())
                 .pipe(gulp.dest('app/js'))
