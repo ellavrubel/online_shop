19a727cafe743b391fbebc9317f6abd8d1d74f56
ellavrubel <webella@protonmail.com>
2020-07-17 12:10:29 +0300

    tabs finished

--- app/js/script.js
+++ app/js/script.js
@@ -12,4 +12,23 @@
         });
 
 
+    //    tabs - main page
+
+        $('.search__tabs-item').on('click', function(e) {
+                e.preventDefault();
+
+                $('.search__tabs-item').removeClass('search__tabs-item--active');
+                $('.search__content-item').removeClass('search__content-item--active');
+
+                $(this).addClass('search__tabs-item--active');
+                $($(this).attr('href')).addClass('search__content-item--active');
+
+
+        });
+
+
+
+
+
+
     });
