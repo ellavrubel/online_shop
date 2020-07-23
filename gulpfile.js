dd127a8606e02f37ca34642cc655557f6b17147a
ellavrubel <webella@protonmail.com>
2020-07-23 18:20:37 +0300

    check-box plugins

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,10 @@
         gulp.task('css', function () {
             return gulp.src([
                 'node_modules/normalize.css/normalize.css',
-                'node_modules/slick-carousel/slick/slick.scss'
+                'node_modules/slick-carousel/slick/slick.scss',
+                'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+                'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+                'node_modules/ion-rangeslider/css/ion.rangeSlider.min.css'
             ])
                 .pipe(concat('libs.scss'))
                 .pipe(gulp.dest('app/scss'))
@@ -41,7 +44,9 @@
         gulp.task('js', function () {
             return gulp.src([
                 'node_modules/jquery/dist/jquery.min.js',
-                'node_modules/slick-carousel/slick/slick.js'
+                'node_modules/jquery-form-styler/dist/jquery.formstyler.min.js',
+                'node_modules/slick-carousel/slick/slick.js',
+                'node_modules/ion-rangeslider/js/ion.rangeSlider.min.js'
             ])
                 .pipe(concat('libs.min.js'))
                 .pipe(uglify())
@@ -98,4 +103,4 @@
 
         gulp.task('build', gulp.series('clean', 'export'));
 
-        gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
+        gulp.task('default', gulp.parallel('js', 'scss', 'css', 'browser-sync', 'watch'));
