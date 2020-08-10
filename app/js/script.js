12db9ca3b01f0a079c90f496ad4d2d686728951e
ellavrubel <webella@protonmail.com>
2020-08-10 15:37:19 +0300

    adaptive banner finished

--- app/js/script.js
+++ app/js/script.js
@@ -9,7 +9,15 @@
             fade: true,
             cssEase: 'linear',
             prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnLeft"><img src="img/arrow-left.svg" alt=""></button>',
-            nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnRight"><img src="img/arrow-right.svg" alt=""></button>'
+            nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnRight"><img src="img/arrow-right.svg" alt=""></button>',
+            responsive: [
+                {
+                    breakpoint: 968,
+                    settings: {
+                        arrows: false
+                    }
+                }
+            ]
 
         });
 
