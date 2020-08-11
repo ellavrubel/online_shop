ef93a13bdf85b9e91f342cb38fa8ab20b24c18bf
ellavrubel <webella@protonmail.com>
2020-08-11 11:34:29 +0300

    adaptive categories finished

--- app/js/script.js
+++ app/js/script.js
@@ -28,7 +28,7 @@
                 e.preventDefault();
 
                 $($(this).siblings()).removeClass('tab--active');
-                $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+                $($(this).parents().siblings().find('div')).removeClass('tabs-content--active');
 
                 $(this).addClass('tab--active');
                 $($(this).attr('href')).addClass('tabs-content--active');
